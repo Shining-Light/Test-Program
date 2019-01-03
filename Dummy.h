5f0d10677d19fa3e9d7feda98ec96c55d8d3a956
Shining-Light <39863281+Shining-Light@users.noreply.github.com>
2019-01-03 10:38:32 -0500

    The Header with shitty classes
    Header.

---
+++ Dummy.h
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <iostream>
+
+class Vector
+{
+	int x, y, z;
+}
+
+class A
+{
+	int integer;
+	float floatvar
+	double doublevar;
+	DWORD dwordvar;
+	Vector Vectorvar;
+	BYTE bytevar;
+	bool boolean = false;
+}
+
+class B
+{
+	int ReturnOne();
+	bool SetOne(int & num);
+	void SetFive(int & num);
+	void ClosePrograms();
+}
