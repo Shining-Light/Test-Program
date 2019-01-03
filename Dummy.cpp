4a58621606f84e7ce62d3cab1ccd64f332feb3a2
Shining-Light <39863281+Shining-Light@users.noreply.github.com>
2019-01-03 10:58:20 -0500

    Create Dummy.cpp

---
+++ Dummy.cpp
@@ -0,0 +1,30 @@
+#include "Dummy.h"
+
+bool booleanfunc(int& y)
+{
+	y = 3;
+	printf(" epic boolean function %d", y);
+	int x = 2;
+	return x > 5;
+}
+void voidfunc()
+{
+	int x = 5;
+	String str = "Epic void func lol";
+	printf("%s %d", str, x);
+}
+
+int main()
+{
+	A Aclass;
+	B Bclass;
+	
+	while (!GetKeyState(VK_F10) &0x8000)
+	{
+		
+		
+		std::cout << std::hex << std::uppercase << x << std::nouppercase << std::dec << std::endl;
+		Sleep(0);
+	}
+	return EXIT_SUCCESS;
+}
