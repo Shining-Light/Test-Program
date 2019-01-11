82fa97e90c4914c8e7428dbcddc1934d40e77c9e
Shining-Light <39863281+Shining-Light@users.noreply.github.com>
2019-01-11 10:50:27 -0500

    Update Dummy.cpp

--- Dummy.cpp
+++ Dummy.cpp
@@ -13,23 +13,54 @@ void voidfunc()
 	String str = "Epic void func lol";
 	printf("%s %d", str, x);
 }
+#define KillKey VK_F10; //kill progrm key
+void printAdress(DWORD Address, std::string funcname)
+{
+	funcname += ": ";
+	std::cout << funcname;
+	std::cout << std::hex << std::uppercase << Address << std::nouppercase << std::dec << std::endl;
+}
 
 int main()
 {
 	A Aclass;
 	B Bclass;
 	
-	while (!(GetKeyState(VK_F10) & 0x8000))
+	std::string anwser;
+	std::cout << "Do want the values of functions to be recurring(y/n)? ";
+	std::cin >> anwser;
+	
+	bool recurring = (anwser == "y");
+	
+	int integer = 5;'
+	float floatval = 12.34;
+	double doubleval = 293.829985839;
+	const char* charArray = "Array";
+	std::string stringtext = "string";
+	
+	
+	while (!(GetKeyState(KillKey) & 0x8000)) //this is aout kill function key so we can kill the program
 	{
-		
-		
-		
-		std::cout << "BClass: " << std::endl;
-		std::cout << std::hex << std::uppercase << *(DWORD*)AClass << std::nouppercase << std::dec << std::endl;
-		std::cout << "AClass: " << std::endl;
-		std::cout << std::hex << std::uppercase << *(DWORD*)AClass << std::nouppercase << std::dec << std::endl;
-		system("CLS");
-		Sleep(0);
+		if (recurring)
+		{
+			integer = 5;'
+			floatval = 12.34;
+			doubleval = 293.829985839;
+			charArray = "Array";
+			stringtext = "string";
+		}
+		//------------------------Display Addresses here for the program----------------------//
+		std::cout << "ADDRESSES:\n";
+		if (recurring) std::cout << "Values Recurring\n";
+		printAdress(*(DWORD*)AClass, "AClass"); std::cout << " | Value: " << "undefined\n";
+		printAdress(*(DWORD*)BClass, "AClass"); std::cout << " | Value: " << "undefined\n";
+		printAdress(*(DWORD*)integer, "integer"); std::cout << " | Value: " << integer << "\n";
+		printAdress(*(DWORD*)floatval, "floatval"); std::cout << " | Value: " << floatval << "\n";
+		printAdress(*(DWORD*)doubleval, "doubleval"); std::cout << " | Value: " << doubleval << "\n";
+		printAdress(*(DWORD*)charArray, "charArray"); std::cout << " | Value: " << charArray.c_str() << "\n";
+		printAdress(*(DWORD*)stringtext, "String text); std::cout << " | Value: " << stringtext << "\n";
+		system("CLS"); //clear console so that we can see the value changing
+		Sleep(0); //sleep for some milliseconds, IDK maybe help the CPU?
 	}
 	return EXIT_SUCCESS;
 }
